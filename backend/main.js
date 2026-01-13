f19ac2f7d4f25e90dff66bfc34e221d20bc657aa
SHIVANSH YADAV <shivanshsdp@gmail.com>
2026-01-14 00:31:04 +0530

    URL RELATED

--- backend/main.js
+++ backend/main.js
@@ -13,7 +13,7 @@ initiateDb();
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
         credentials: true,
     })
 );
